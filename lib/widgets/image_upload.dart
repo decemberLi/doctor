7820f0153661de323aced8779f8390e03a531987
caolong <cmlily8294@gmail.com>
2020-11-09 20:28:37 +0800

    feat:上传图片

--- lib/widgets/image_upload.dart
+++ lib/widgets/image_upload.dart
@@ -21,12 +21,15 @@ class ImageUpload extends StatefulWidget {
   /// 自定义上传后的图类别
   final String customUploadImageType;
 
+  final int maxLength;
+
   ImageUpload({
     this.images = const <OssFileEntity>[],
     this.width = 74,
     this.height = 60,
     this.onChange,
     this.customUploadImageType,
+    this.maxLength = 3,
   });
 
   @override
@@ -47,14 +50,12 @@ class _ImageUploadState extends State<ImageUpload> {
   @override
   void initState() {
     this.initialize();
-    print('$ImageUpload --- initState');
     super.initState();
   }
 
   @override
   void didUpdateWidget(ImageUpload oldWidget) {
     this.initialize();
-    print('$ImageUpload --- didUpdateWidget');
     super.didUpdateWidget(oldWidget);
   }
 
@@ -89,26 +90,12 @@ class _ImageUploadState extends State<ImageUpload> {
     }
   }
 
-  Widget _imageWrapper({
-    child,
-  }) {
-    return Container(
-      width: widget.width,
-      height: widget.height,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: ThemeColor.colorLine,
-        ),
-      ),
-      child: child,
-    );
-  }
-
   /// 上传按钮
   Widget _uploadBtn() {
     return GestureDetector(
-      child: _imageWrapper(
+      child: ImageItemWrapper(
+        width: widget.width,
+        height: widget.height,
         child: Icon(
           Icons.add,
           color: ThemeColor.colorLine,
@@ -118,74 +105,16 @@ class _ImageUploadState extends State<ImageUpload> {
     );
   }
 
-  Future<OssFileEntity> _getImageUrlByOssId(OssFileEntity image) async {
-    if (image.url == null) {
-      var data = await OssService.getFile({
-        'ossIds': [image.ossId]
-      });
-      if (data.isNotEmpty) {
-        image.url = data[0]['tmpUrl'];
-        image.type = widget.customUploadImageType ?? data[0]['type'];
-      }
-    }
-    return image;
-  }
-
   Widget renderImageItem(OssFileEntity image) {
     return Container(
       child: Stack(
         overflow: Overflow.visible,
         children: [
-          FutureBuilder(
-            future: _getImageUrlByOssId(image),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done &&
-                  image.url != null) {
-                ImageProvider imageProvider = NetworkImage(image.url);
-                Image imageWidget = Image(
-                  image: imageProvider,
-                  width: widget.width,
-                  height: widget.height,
-                  alignment: Alignment.topCenter,
-                  fit: BoxFit.fitHeight,
-                  loadingBuilder: (BuildContext context, Widget child,
-                      ImageChunkEvent loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return _imageWrapper(
-                      child: LinearPercentIndicator(
-                        percent: loadingProgress.expectedTotalBytes != null
-                            ? loadingProgress.cumulativeBytesLoaded /
-                                loadingProgress.expectedTotalBytes
-                            : 0.0,
-                        progressColor: ThemeColor.primaryColor,
-                      ),
-                    );
-                  },
-                );
-                return GestureDetector(
-                  child: imageWidget,
-                  onTap: () {
-                    Navigator.of(context).push(
-                      FadeRoute(
-                        // page: PhotoViewSimpleScreen(
-                        //   imageProvider: imageProvider,
-                        //   heroTag: 'simple',
-                        // ),
-                        page: PhotoViewGalleryScreen(
-                          images: this._images.map((e) => e.url).toList(),
-                        ),
-                      ),
-                    );
-                  },
-                );
-              }
-              return _imageWrapper(
-                child: LinearPercentIndicator(
-                  percent: 0.0,
-                  progressColor: ThemeColor.primaryColor,
-                ),
-              );
-            },
+          ImageItem(
+            image: image,
+            width: widget.width,
+            height: widget.height,
+            customUploadImageType: widget.customUploadImageType,
           ),
           Positioned(
             top: -16.0,
@@ -225,9 +154,138 @@ class _ImageUploadState extends State<ImageUpload> {
         runSpacing: 16,
         children: [
           ..._images.map((e) => renderImageItem(e)).toList(),
-          _uploadBtn(),
+          if (_images.length < widget.maxLength) _uploadBtn(),
         ],
       ),
+    );
+  }
+}
+
+class ImageItemWrapper extends StatelessWidget {
+  final Widget child;
+  final double width;
+  final double height;
+  ImageItemWrapper({
+    this.child,
+    this.width,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: this.width,
+      height: this.height,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: ThemeColor.colorLine,
+        ),
+      ),
+      child: child,
+    );
+  }
+}
+
+class ImageItem extends StatefulWidget {
+  /// 自定义上传后的图类别
+  final String customUploadImageType;
+
+  final OssFileEntity image;
+
+  final double width;
+
+  final double height;
+
+  ImageItem({
+    this.image,
+    this.customUploadImageType,
+    this.width,
+    this.height,
+  });
+
+  @override
+  _ImageItemState createState() => _ImageItemState();
+}
+
+class _ImageItemState extends State<ImageItem> {
+  Future<OssFileEntity> _futureBuilderFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureBuilderFuture = _getImageUrlByOssId(widget.image);
+  }
+
+  Future<OssFileEntity> _getImageUrlByOssId(OssFileEntity image) async {
+    if (image.url == null) {
+      var data = await OssService.getFile({
+        'ossIds': [image.ossId]
+      });
+      if (data.isNotEmpty) {
+        image.url = data[0]['tmpUrl'];
+        image.type = widget.customUploadImageType ?? data[0]['type'];
+      }
+    }
+    return image;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _futureBuilderFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            widget.image.url != null) {
+          ImageProvider imageProvider = NetworkImage(widget.image.url);
+          Image imageWidget = Image(
+            image: imageProvider,
+            width: widget.width,
+            height: widget.height,
+            alignment: Alignment.topCenter,
+            fit: BoxFit.fitHeight,
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent loadingProgress) {
+              if (loadingProgress == null) return child;
+              return ImageItemWrapper(
+                width: widget.width,
+                height: widget.height,
+                child: LinearPercentIndicator(
+                  percent: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes
+                      : 0.0,
+                  progressColor: ThemeColor.primaryColor,
+                ),
+              );
+            },
+          );
+          return GestureDetector(
+            child: imageWidget,
+            onTap: () {
+              Navigator.of(context).push(
+                FadeRoute(
+                  page: PhotoViewSimpleScreen(
+                    imageProvider: imageProvider,
+                    heroTag: 'simple',
+                  ),
+                  // page: PhotoViewGalleryScreen(
+                  //   images: this._images.map((e) => e.url).toList(),
+                  // ),
+                ),
+              );
+            },
+          );
+        }
+        return ImageItemWrapper(
+          width: widget.width,
+          height: widget.height,
+          child: LinearPercentIndicator(
+            percent: 0.0,
+            progressColor: ThemeColor.primaryColor,
+          ),
+        );
+      },
     );
   }
 }
