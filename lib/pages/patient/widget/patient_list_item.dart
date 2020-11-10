5574e76a8bf8dfd946988401e6a06bf95d7129ca
yetian <hideinwolves@163.com>
2020-11-10 11:16:54 +0800

    fix:我的患者ui

--- lib/pages/patient/widget/patient_list_item.dart
+++ lib/pages/patient/widget/patient_list_item.dart
@@ -1,7 +1,6 @@
 import 'package:common_utils/common_utils.dart';
 import 'package:doctor/pages/patient/model/patient_model.dart';
 import 'package:doctor/theme/common_style.dart';
-import 'package:doctor/widgets/ace_button.dart';
 import 'package:flutter/material.dart';
 
 class PatientListItem extends StatelessWidget {
@@ -13,46 +12,49 @@ class PatientListItem extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 12.0),
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 23.0, vertical: 20.0),
-        alignment: Alignment.topLeft,
         child: Column(
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.asset(
-                        'assets/images/avatar.png',
-                        width: 42,
-                        height: 42,
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(left: 9),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              data.patientName ?? '',
-                              style: MyStyles.boldTextStyle_16,
-                            ),
-                            Text(
-                              '${data.sexLabel} | ${data.age}岁',
-                              style: MyStyles.inputTextStyle_12_grey,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                  child: Image.asset(
+                    'assets/images/avatar.png',
+                    width: 42,
+                    height: 42,
                   ),
                 ),
-                Text(
-                  '${DateUtil.formatDateMs(data.diagnosisTime, format: 'yyyy.MM.dd HH:mm')}',
-                  style: MyStyles.inputTextStyle_12_grey,
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.only(left: 9),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                data.patientName ?? '',
+                                style: MyStyles.boldTextStyle_16,
+                              ),
+                              Text(
+                                '${DateUtil.formatDateMs(data.diagnosisTime, format: 'yyyy.MM.dd HH:mm')}',
+                                style: MyStyles.inputTextStyle_12_grey,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            '${data.sexLabel} | ${data.age}岁',
+                            style: MyStyles.inputTextStyle_12_grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
