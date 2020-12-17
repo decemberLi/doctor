148a64e3322854b39027544cec139c9ce5f97d0d
dec <252539361@qq.com>
2020-12-17 18:15:01 +0800

    修改了easyloading的错误处理

--- lib/widgets/YYYEasyLoading.dart
+++ lib/widgets/YYYEasyLoading.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 extension falshEx on EasyLoading {
-  static flash(Future Function() load,{String text = "加载中..."})async {
+  flash(Future Function() load, {String text = "加载中..."}) async {
     EasyLoading.show(status: text);
-    await load();
-    EasyLoading.dismiss();
+    try {
+      await load();
+    } catch (e) {
+      rethrow;
+    } finally {
+      EasyLoading.dismiss();
+    }
   }
 }
