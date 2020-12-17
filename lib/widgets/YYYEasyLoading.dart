380aae6ef6df172f80daf2550b514c322409a6d6
dec <252539361@qq.com>
2020-12-17 17:50:23 +0800

    flash添加文字

--- lib/widgets/YYYEasyLoading.dart
+++ lib/widgets/YYYEasyLoading.dart
@@ -2,7 +2,7 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 extension falshEx on EasyLoading {
   static flash(Future Function() load,{String text = "加载中..."})async {
-    EasyLoading.show();
+    EasyLoading.show(status: text);
     await load();
     EasyLoading.dismiss();
   }
