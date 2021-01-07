f7fa27764ea906c66f0b72d7f3d462f76201606d
liuchangqing <liuchangqing@e-medclouds.com>
2021-01-07 17:04:01 +0800

    feat: 视频录制纯UI

---
+++ android/app/src/main/kotlin/com/emedclouds/doctor/pages/learning/LessonRecordActivity.kt
@@ -0,0 +1,15 @@
+package com.emedclouds.doctor.pages.learning
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.emedclouds.doctor.R
+
+class LessonRecordActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson_record_layout)
+    }
+
+}
