78c948e96aa90a4be165298a47cd68d6ac58768c
dec <252539361@qq.com>
2021-01-08 17:38:50 +0800

    完善了权限提醒

--- ios/Runner/HUD.swift
+++ ios/Runner/HUD.swift
@@ -22,4 +22,16 @@ extension MBProgressHUD {
         hud.bezelView.style = .solidColor
         hud.hide(animated: true, afterDelay: 1)
     }
+    static func toastText(msg : String) {
+        guard let window = AppDelegate.shared?.window else {return}
+        let hud = MBProgressHUD.showAdded(to: window, animated: true)
+        hud.label.text = msg
+        hud.label.textColor = .white
+        hud.mode = .text
+        hud.contentColor = .white
+        hud.bezelView.backgroundColor = .black
+        hud.bezelView.color = UIColor(white: 0, alpha: 0.7)
+        hud.bezelView.style = .solidColor
+        hud.hide(animated: true, afterDelay: 1)
+    }
 }
