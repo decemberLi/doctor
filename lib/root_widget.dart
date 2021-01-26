7268744f810ff919dc88e1dfb156093e1948c56b
dec <252539361@qq.com>
2021-01-26 16:44:18 +0800

    完善了通知跳转

--- lib/root_widget.dart
+++ lib/root_widget.dart
@@ -3,12 +3,14 @@ import 'dart:convert';
 import 'package:device_info/device_info.dart';
 import 'package:doctor/pages/home_page.dart';
 import 'package:doctor/pages/login/login_by_chaptcha.dart';
+import 'package:doctor/pages/qualification/doctor_physician_status_page.dart';
 import 'package:doctor/provider/provider_manager.dart';
 import 'package:doctor/route/navigation_service.dart';
 import 'package:doctor/route/route_manager.dart';
 import 'package:doctor/theme/theme.dart';
 import 'package:doctor/utils/MedcloudsNativeApi.dart';
 import 'package:doctor/utils/constants.dart';
+import 'package:doctor/utils/platform_utils.dart';
 import 'package:event_bus/event_bus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -18,6 +20,9 @@ import 'package:http_manager/manager.dart';
 import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'dart:io';
+
+import 'model/ucenter/doctor_detail_info_entity.dart';
+import 'package:doctor/http/ucenter.dart';
 
 
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
@@ -51,29 +56,38 @@ class RootWidget extends StatelessWidget {
         if (type == "QUALIFICATION_AUTH") { // 资质认证
           var authStatus = obj["authStatus"];
           if (authStatus == "FAIL"){
-
+            var basicData = await API.shared.ucenter.getBasicData();
+            var doctorData = DoctorDetailInfoEntity.fromJson(basicData);
+            Navigator.pushNamed(
+              context,
+              RouteManager.USERINFO_DETAIL,
+              arguments: {
+                'doctorData': doctorData.toJson(),
+                'qualification': true,
+              },
+            );
           }else{
-
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DoctorPhysicianStatusPage(
+                        authStatus)));
           }
           Navigator.of(context).pushNamed("routeName");
         }else if (type == "ASSIGN_STUDY_PLAN") { // 学习计划详情
           var learnPlanId = obj["learnPlanId"];
-          var learnStatus = obj["learnStatus"];
           Navigator.of(context).pushNamed(
             RouteManager.LEARN_DETAIL,
             arguments: {
               'learnPlanId': learnPlanId,
-              'listStatus': learnStatus,
             },
           );
         }else if (type == "RELEARN"){ // 学习计划详情
           var learnPlanId = obj["learnPlanId"];
-          var learnStatus = obj["learnStatus"];
           Navigator.of(context).pushNamed(
             RouteManager.LEARN_DETAIL,
             arguments: {
               'learnPlanId': learnPlanId,
-              'listStatus': learnStatus,
             },
           );
         }
@@ -87,14 +101,7 @@ class RootWidget extends StatelessWidget {
       debugPrint("$options");
       debugPrint("ticket:${SessionManager.shared.session}");
       options.headers["_ticketObject"] = SessionManager.shared.session;
-      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-      if (Platform.isIOS) {
-        IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
-        options.headers["_appVersion"] = iosInfo.utsname.release;
-      } else if (Platform.isAndroid){
-        AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-        options.headers["_appVersion"] = androidInfo.version.release;
-      }
+      options.headers["_appVersion"] =  await PlatformUtils.getAppVersion();
 
       return options;
     };
