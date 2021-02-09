72e6dce04ecce5de87f4b582d89659028c48b502
dec <252539361@qq.com>
2021-02-09 10:31:33 +0800

    修改了医生圈2级列表的图片比例

--- lib/pages/doctors/doctors_list_page2.dart
+++ lib/pages/doctors/doctors_list_page2.dart
@@ -231,10 +231,10 @@ class DoctorsListCell2 extends StatelessWidget {
                 ),
                 Container(
                   color: Color(0xffEAF3FF),
-                  width: 116,
+                  width: 133,
                   height: 80,
                   child: Image.network("${data.coverUrl}",
-                      width: 116, height: 80, fit: BoxFit.cover),
+                      width: 133, height: 80, fit: BoxFit.cover),
                 )
               ],
             ),
