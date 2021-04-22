948934a0dcaeefe0d9eb9947a30bd885d5c9c36e
dec <252539361@qq.com>
2021-04-22 10:40:31 +0800

    修改了点赞通知和评论通知为normalTable

--- lib/pages/message/widget/comment_message_list_widget.dart
+++ lib/pages/message/widget/comment_message_list_widget.dart
@@ -1,10 +1,12 @@
 import 'package:doctor/pages/message/view_model/social_message_list_view_model.dart';
 import 'package:doctor/pages/message/widget/redoc_logic.dart';
+import 'package:doctor/provider/view_state_widget.dart';
 import 'package:doctor/route/route_manager.dart';
 import 'package:doctor/theme/theme.dart';
 import 'package:doctor/utils/data_format_util.dart';
 import 'package:doctor/widgets/common_widget_style.dart';
 import 'package:doctor/widgets/refreshable_list_widget.dart';
+import 'package:doctor/widgets/table_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -15,8 +17,7 @@ class CommentMessagePage extends StatefulWidget {
   State<StatefulWidget> createState() => _CommentMessagePageState();
 }
 
-class _CommentMessagePageState extends AbstractListPageState<
-    SocialMessageListViewModel, CommentMessagePage> {
+class _CommentMessagePageState extends State<CommentMessagePage> {
   var textStyle =
       const TextStyle(fontSize: 10, color: ThemeColor.colorFF444444);
 
@@ -30,24 +31,38 @@ class _CommentMessagePageState extends AbstractListPageState<
           style: TextStyle(fontSize: 17, color: ThemeColor.colorFF000000),
         ),
       ),
-      body: super.build(context),
+      body: Container(
+        color: ThemeColor.colorFFF3F5F8,
+        child: NormalTableView(
+          padding: EdgeInsets.only(top: 10),
+          itemBuilder: itemWidget,
+          holder: (isError, message) {
+            if (isError) {
+              return ViewStateEmptyWidget(message: message);
+            } else {
+              return ViewStateEmptyWidget(message: '还没有任何评论，好落寞');
+            }
+          },
+          getData: (page) async {
+            if (page == 1) {
+              await _model.initData();
+            } else {
+              await _model.loadData(pageNum: page);
+            }
+            return _model.list;
+          },
+        ),
+      ),
     );
   }
 
-  @override
-  Widget emptyWidget(String msg) {
-    return super.emptyWidget('还没有任何评论，好落寞');
-  }
-
-  @override
-  SocialMessageListViewModel getModel() =>
+  SocialMessageListViewModel _model =
       SocialMessageListViewModel(SocialMessageType.TYPE_COMMENT);
 
-  @override
-  Widget itemWidget(BuildContext context, int index, dynamic data) {
-    return Container(
+  Widget itemWidget(BuildContext context, dynamic data) {
+    var content = Container(
       padding: EdgeInsets.all(12),
-      margin: EdgeInsets.symmetric(horizontal: 16),
+      margin: EdgeInsets.symmetric(horizontal: 16,vertical: 5),
       decoration: itemContainerDecoration,
       child: IntrinsicHeight(
         child: Row(
@@ -111,11 +126,16 @@ class _CommentMessagePageState extends AbstractListPageState<
         ),
       ),
     );
+    return GestureDetector(
+      child: content,
+      onTap: () {
+        onItemClicked(data);
+      },
+    );
   }
 
-  @override
-  void onItemClicked(SocialMessageListViewModel model, itemData) {
+  void onItemClicked(itemData) {
     RouteManager.openDoctorsDetail(itemData?.postId, from: "msg");
-    model?.messageClicked(itemData.messageId);
+    _model.messageClicked(itemData.messageId);
   }
 }
