1ae93223b7ee65e4f5bf6448f68a428c19aeec69
liuchangqing <liuchangqing@e-medclouds.com>
2021-05-08 09:58:30 +0800

    feat: 添加活动包

---
+++ lib/pages/activity/activity_list_page.dart
@@ -0,0 +1,19 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+
+class ActivityListPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() =>_ActivityListPageState();
+
+}
+
+class _ActivityListPageState extends State<ActivityListPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
