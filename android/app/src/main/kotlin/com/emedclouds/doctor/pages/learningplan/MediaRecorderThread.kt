7ac9d8beaf5c60678d5351dec94c664120f78ccc
liuchangqing <liuchangqing@e-medclouds.com>
2021-05-18 16:38:02 +0800

    feat: 参数修改

--- android/app/src/main/kotlin/com/emedclouds/doctor/pages/learningplan/MediaRecorderThread.kt
+++ android/app/src/main/kotlin/com/emedclouds/doctor/pages/learningplan/MediaRecorderThread.kt
@@ -57,9 +57,8 @@ class MediaRecorderThread(
         val profile = CamcorderProfile.get(CamcorderProfile.QUALITY_720P)
         mMediaRecorder.apply {
             // 设置视频来源
-            setAudioChannels(2)
             setVideoSource(MediaRecorder.VideoSource.SURFACE)
-            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION)
             setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
             setOutputFile(File(mDstPath, "${mFileSequence++}.mp4").absolutePath)
             setVideoSize(mWidth, mHeight)
